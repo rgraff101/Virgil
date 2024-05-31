e0f0b4809e8a510627302f6989d08a0fcf9b38d1
linzhanguca <lzhang12@uca.edu>
2024-05-31 17:30:59 -0500

    test camera using picam2

--- bear_cart/component_test/camera.py
+++ bear_cart/component_test/camera.py
@@ -1,13 +1,19 @@
+#!/usr/bin/python3
+
 import cv2
 
+from picamera2 import Picamera2
+
+# Grab images as numpy arrays and leave everything else to OpenCV.
 
 cv2.startWindowThread()
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Camera", frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
 
-cv2.destroyAllWindows()
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+picam2.start()
+
+while True:
+    im = picam2.capture_array()
+    grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Camera", im)
+    cv2.waitKey(1)

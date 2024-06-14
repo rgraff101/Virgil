f4510f2a16530ddf7d76c8ce530ebe59cfb49f93
linzhanguca <lzhang12@uca.edu>
2024-06-14 14:40:30 -0500

    update collect_data

--- bear_cart/collect_data.py
+++ bear_cart/collect_data.py
@@ -2,25 +2,40 @@ import sys
 import os
 from datetime import datetime
 import cv2 as cv
+from picamera2 import Picamera2
 import pygame
-from gpiozero import Servo, PhaseEnableMotor
+from gpiozero import AngularServo, PhaseEnableMotor
+import json
 from time import time
 import csv
 
 
 # SETUP
 # Load configs
-THROTTLE_AXIS = 1
-STEER_AXIS = 2
-STEER_CENTER = 0.2
-STEER_OFFSET = 0.7
-STEER_DIR = -1  # 1: steer left if steer.value < 0; -1: steer left if steer.value > 0. 
-THROTTLE_LIMIT = 0.3
+params_file_path = os.path.join(sys.path[0], 'configs.json')
+params_file = open(params_file_path)
+params = json.load(params_file)
+# Constants
+STEER_AXIS = params['steer_joy_axis']
+THROTTLE_AXIS = params['throttle_joy_axis']
+RECORD_BUTTON = params['record_btn']
+STOP_BUTTON = params['stop_btn']
+STEER_CENTER = params['steer_center_angle']
+STEER_RANGE = params['steer_range']
+STEER_DIR = params['steer_dir']
+THROTTLE_LIMIT = params['throttle_limit']
 # Init servo 
-steer = Servo(pin=17)
-steer.value = STEER_CENTER #Starting angle
+steer = AngularServo(
+    pin=params['steer_pin'], 
+    initial_angle=params['steer_center_angle'], 
+    min_angle=params['steer_min_angle'], 
+    max_angle=params['steer_max_angle'],
+)
 # Init motor 
-throttle = PhaseEnableMotor(phase=19, enable=26)
+throttle = PhaseEnableMotor(
+    phase=params['throttle_dir_pin'], 
+    enable=params['throttle_pwm_pin'],
+)
 # Init controller
 pygame.display.init()
 pygame.joystick.init()
@@ -35,15 +50,23 @@ if not os.path.exists(image_dir):
             raise
 label_path = os.path.join(os.path.dirname(os.path.dirname(image_dir)), 'labels.csv')
 # Init camera
-cap = cv.VideoCapture(2)
-cap.set(cv.CAP_PROP_FPS, 20)
+cam = Picamera2()
+cam.configure(
+    cam.create_preview_configuration(
+        main={"format": 'RGB888', "size": (120, 160)},
+        controls={"FrameDurationLimits": (50000, 50000)},  # 20 FPS
+    )
+)
+cam.start()
 for i in reversed(range(60)):
-    ret, frame = cap.read()
-    if not ret:
+    frame = cam.capture_array()
+    # cv.imshow("Camera", frame)
+    # cv.waitKey(1)
+    if frame is None:
         print("No frame received. TERMINATE!")
         sys.exit()
     if not i % 20:
-        print(i/20)  # count down 3, 2, 1 sec   
+        print(i/20)  # count down 3, 2, 1 sec
 # Init timer for FPS computing
 start_stamp = time()
 frame_counts = 0
@@ -55,17 +78,16 @@ is_recording = False
 # MAIN LOOP
 try:
     while True:
-        ret, frame = cap.read()  # read image
-        if not ret:
+        frame = cam.capture_array()  # read image
+        if frame is None:
             print("No frame received. TERMINATE!")
             sys.exit()
-        # cv.imshow('camera', cv.resize(frame, (320, 240)))
         for e in pygame.event.get():  # read controller input
             if e.type == pygame.JOYAXISMOTION:
                 st_ax_val = round((js.get_axis(STEER_AXIS)), 2)  
                 th_ax_val = round((js.get_axis(THROTTLE_AXIS)), 2)  # keep 2 decimals
             elif e.type == pygame.JOYBUTTONDOWN:
-                if js.get_button(11):  # START button 
+                if js.get_button(STOP_BUTTON):  # emergency stop 
                     throttle.stop()
                     throttle.close()
                     steer.close()
@@ -73,13 +95,14 @@ try:
                     pygame.quit()
                     print("E-STOP PRESSED. TERMINATE")
                     sys.exit()
-                elif js.get_button(0):  # A button
+                elif js.get_button(RECORD_BUTTON):
                     is_recording = not is_recording
+                    print(f"Recording: {is_recording}")
         # Calaculate steering and throttle value
-        act_st = st_ax_val  # steer_input: -1: left, 1: right
-        act_th = -th_ax_val  # throttle input: -1: max forward, 1: max backward
+        act_st = st_ax_val  # steer action: -1: left, 1: right
+        act_th = -th_ax_val  # throttle action: -1: max forward, 1: max backward
         # Drive servo
-        steer.value = STEER_CENTER + act_st * STEER_OFFSET * STEER_DIR
+        steer.angle = STEER_CENTER + act_st * STEER_RANGE * STEER_DIR
         # Drive motor
         if act_th >= 0.1:
             throttle.forward(min(act_th, THROTTLE_LIMIT))
@@ -91,8 +114,8 @@ try:
         action = [act_st, act_th]
         print(f"action: {action}")
         if is_recording:
-            img = cv.resize(frame, (120, 160))
-            cv.imwrite(image_dir + str(frame_counts) + '.jpg', img)
+            # img = cv.resize(frame, (120, 160))
+            cv.imwrite(image_dir + str(frame_counts) + '.jpg', frame)
             label = [str(frame_counts) + '.jpg'] + action
             with open(label_path, 'a+', newline='') as f:
                 writer = csv.writer(f)

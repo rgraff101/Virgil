cdb7e8996410c5cc4ee785d82fa9b3e9948fbe57
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2024-06-18 22:18:06 -0500

    Update servo_calibrate.py

--- bear_cart/component_test/servo_calibrate.py
+++ bear_cart/component_test/servo_calibrate.py
@@ -1,17 +1,35 @@
 """
 Start with initial_angle=0, min_angle=-90, max_angle=90
 Observe and log calibrated angle values below:
-    left most angle: -30
-    right most angle: 90
+    left most angle: 
+    right most angle: 
     center: 30
 Hint: running in Python's interactive mode
 """
+import sys
+import os
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(12, initial_angle=30, min_angle=-90, max_angle=90)
+# Load configs
+params_file_path = os.path.join(os.path.dirname(sys.path[0]), 'configs.json')
+params_file = open(params_file_path)
+params = json.load(params_file)
 
-for ang in range(-30, 90, 5):
+# SETUP
+print("Please take down the angle values when front wheels are turned all the to the left, right and centered.")
+for i in reversed(range(1, 4)):
+    print(i)
+    sleep(1)
+servo = AngularServo(
+    pin=params['steer_pin'], 
+    initial_angle=params['steer_center_angle'], 
+    min_angle=params['steer_min_angle'], 
+    max_angle=params['steer_max_angle'],
+)
+
+# LOOP
+for ang in range(-90, 90, 5):
     servo.angle = ang
     print(ang)
     sleep(.5)

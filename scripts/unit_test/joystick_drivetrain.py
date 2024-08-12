074f55cb71569df600ad552c2b4b98503e0326ec
linzhanguca <lzhang12@uca.edu>
2024-08-12 15:48:35 -0500

    update joystick drivetrain control test

--- scripts/unit_test/joystick_drivetrain.py
+++ scripts/unit_test/joystick_drivetrain.py
@@ -16,27 +16,32 @@ params_file = open(params_file_path)
 params = json.load(params_file)
 # Constants
 STEERING_AXIS = params['steering_joy_axis']
+STEERING_CENTER = params['steering_center']
+STEERING_RANGE = params['steering_range']
 THROTTLE_AXIS = params['throttle_joy_axis']
+THROTTLE_STALL = params['throttle_stall']
+THROTTLE_FWD_RANGE = params['throttle_fwd_range']
+THROTTLE_REV_RANGE = params['throttle_rev_range']
 STOP_BUTTON = params['stop_btn']
 TROTTLE_LIMIT = params['throttle_limit']
 # Init serial port
 ser_pico = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
-print(f"Connection to port {ser_pico.name} established.")
+print(f"Pico is connected to port: {ser.name}")
 # Init controller
 pygame.display.init()
 pygame.joystick.init()
 js = pygame.joystick.Joystick(0)
 # Init joystick axes values
-st_ax_val = 0.
-th_ax_val = 0.
+ax_val_st = 0.
+ax_val_th = 0.
 
 # MAIN LOOP
 try:
     while True:
         for e in pygame.event.get():  # read controller input
             if e.type == pygame.JOYAXISMOTION:
-                st_ax_val = round((js.get_axis(STEERING_AXIS)), 2)  # keep 2 decimals
-                th_ax_val = round((js.get_axis(THROTTLE_AXIS)), 2)  # keep 2 decimals
+                ax_val_st = round((js.get_axis(STEERING_AXIS)), 2)  # keep 2 decimals
+                ax_val_th = round((js.get_axis(THROTTLE_AXIS)), 2)  # keep 2 decimals
             elif e.type == pygame.JOYBUTTONDOWN:
                 if js.get_button(STOP_BUTTON):  # emergency stop 
                     print("E-STOP PRESSED. TERMINATE")
@@ -45,10 +50,18 @@ try:
                     sys.exit()
         # Calaculate steering and throttle value
         # TODO: set throttle limit
-        act_st = st_ax_val
-        act_th = -th_ax_val  # throttle action: -1: max forward, 1: max backward
-        # Drive motor
-        msg = (str(act_st) + "," + str(act_th) + "\n").encode('utf-8')
+        act_st = ax_val_st
+        act_th = -ax_val_th  # throttle action: -1: max forward, 1: max backward
+        # Encode steering value to dutycycle in nanosecond
+        duty_st = STEERING_CENTER - STEERING_RANGE + int(STEERING_RANGE * (act_st + 1))
+        # Encode throttle value to dutycycle in nanosecond
+        if act_th > 0:
+            duty_th = THROTTLE_STALL + int(THROTTLE_FWD_RANGE * act_th)
+        elif act_th < 0:
+            duty_th = THROTTLE_STALL + int(THROTTLE_REV_RANGE * act_th)
+        else:
+            duty_th = THROTTLE_STALL 
+        msg = (str(duty_st) + "," + str(duty_th) + "\n").encode('utf-8')
         # f"{act_st,act_th}\n".encode('utf-8')
         ser_pico.write(msg)
         # Log action
--- scripts/unit_test/serial_throttle.py
+++ scripts/unit_test/serial_throttle.py
@@ -15,8 +15,8 @@ params_file = open(params_file_path)
 params = json.load(params_file)
 STEERING_CENTER = params['steering_center']
 THROTTLE_STALL = params['throttle_stall']
-THROTTLE_FWD_MAX = params['throttle_fwd_max']
-THROTTLE_REV_MAX = params['throttle_rev_max']
+THROTTLE_FWD_RANGE = params['throttle_fwd_range']
+THROTTLE_REV_RANGE = params['throttle_rev_range']
 THROTTLE_LIMIT = params['throttle_limit']
 # Init serial port
 ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
@@ -28,8 +28,8 @@ duty_th = THROTTLE_STALL
 
 # LOOP
 # Throttle: stall->fwd->stall->rev->stall
-for i in range(100):
-    duty_th = THROTTLE_STALL + int((THROTTLE_FWD_MAX - THROTTLE_STALL) * (i/100))
+for i in range(100): # forward ramp up
+    duty_th = THROTTLE_STALL + int(THROTTLE_FWD_RANGE * i / 100)
     msg = (str(duty_st) + "," + str(duty_th) + "\n").encode('utf-8')
     # msg = f"{i/100}\n".encode('utf-8')
     ser.write(msg)
@@ -38,8 +38,8 @@ for i in range(100):
         pico_data = ser.readline()
         pico_data = pico_data.decode('utf-8', 'ignore')
         print(pico_data)
-for i in reversed(range(100)):
-    duty_th = THROTTLE_STALL + int((THROTTLE_FWD_MAX - THROTTLE_STALL) * (i/100))
+for i in reversed(range(100)): # forward ramp down
+    duty_th = THROTTLE_STALL + int(THROTTLE_FWD_RANGE * i / 100)
     msg = (str(duty_st) + "," + str(duty_th) + "\n").encode('utf-8')
     # msg = f"{i/100}\n".encode('utf-8')
     ser.write(msg)
@@ -48,8 +48,8 @@ for i in reversed(range(100)):
         pico_data = ser.readline()
         pico_data = pico_data.decode('utf-8', 'ignore')
         print(pico_data)
-for i in range(100):
-    duty_th = THROTTLE_STALL - int((THROTTLE_STALL - THROTTLE_REV_MAX) * (i/100))
+for i in range(100): # reverse ramp up
+    duty_th = THROTTLE_STALL - int(THROTTLE_REV_RANGE * i / 100)
     msg = (str(duty_st) + "," + str(duty_th) + "\n").encode('utf-8')
     # msg = f"{-i/100}\n".encode('utf-8')
     ser.write(msg)
@@ -58,8 +58,8 @@ for i in range(100):
         pico_data = ser.readline()
         pico_data = pico_data.decode('utf-8', 'ignore')
         print(pico_data)
-for i in reversed(range(100)):
-    duty_th = THROTTLE_STALL - int((THROTTLE_STALL - THROTTLE_REV_MAX) * (i/100))
+for i in reversed(range(100)): # reverse ramp down
+    duty_th = THROTTLE_STALL - int(THROTTLE_REV_RANGE * i / 100)
     msg = (str(duty_st) + "," + str(duty_th) + "\n").encode('utf-8')
     # msg = f"{-i/100}\n".encode('utf-8')
     ser.write(msg)

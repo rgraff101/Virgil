9b7409d40f4b94a04b058b07d55b2fa9bb620ef4
linzhanguca <lzhang12@uca.edu>
2024-08-13 11:45:29 -0500

    add new integrated test
    camera, joystick, drivetrain tested together

---
+++ scripts/unit_test/camera_joystick_drivetrain.py
@@ -0,0 +1,119 @@
+"""
+Integrated test with controller, pico usb communication, throttle motor.
+"""
+import sys
+import os
+import serial
+import cv2 as cv
+from picamera2 import Picamera2
+import pygame
+import json
+from time import time
+
+
+# SETUP
+# Load configs
+params_file_path = os.path.join(os.path.dirname(sys.path[0]), 'configs.json')
+params_file = open(params_file_path)
+params = json.load(params_file)
+# Constants
+STEERING_AXIS = params['steering_joy_axis']
+STEERING_CENTER = params['steering_center']
+STEERING_RANGE = params['steering_range']
+THROTTLE_AXIS = params['throttle_joy_axis']
+THROTTLE_STALL = params['throttle_stall']
+THROTTLE_FWD_RANGE = params['throttle_fwd_range']
+THROTTLE_REV_RANGE = params['throttle_rev_range']
+THROTTLE_LIMIT = params['throttle_limit']
+STOP_BUTTON = params['stop_btn']
+# Init serial port
+ser_pico = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
+print(f"Pico is connected to port: {ser_pico.name}")
+# Init controller
+pygame.display.init()
+pygame.joystick.init()
+js = pygame.joystick.Joystick(0)
+# Init camera
+cv.startWindowThread()
+cam = Picamera2()
+cam.configure(
+    cam.create_preview_configuration(
+        main={"format": 'RGB888', "size": (120, 160)},
+        controls={"FrameDurationLimits": (50000, 50000)},  # 20 FPS
+    )
+)
+cam.start()
+for i in reversed(range(60)):
+    frame = cam.capture_array()
+    # cv.imshow("Camera", frame)
+    # cv.waitKey(1)
+    if frame is None:
+        print("No frame received. TERMINATE!")
+        sys.exit()
+    if not i % 20:
+        print(i/20)  # count down 3, 2, 1 sec
+# Init timer for FPS computing
+start_stamp = time()
+frame_counts = 0
+ave_frame_rate = 0.
+# Init joystick axes values
+ax_val_st = 0.
+ax_val_th = 0.
+
+# MAIN LOOP
+try:
+    while True:
+        frame = cam.capture_array()  # read image
+        if frame is None:
+            print("No frame received. TERMINATE!")
+            cv.destroyAllWindows()
+            pygame.quit()
+            ser_pico.close()
+            sys.exit()
+        cv.imshow('camera', frame)
+        for e in pygame.event.get():  # read controller input
+            if e.type == pygame.JOYAXISMOTION:
+                ax_val_st = round((js.get_axis(STEERING_AXIS)), 2)  # keep 2 decimals
+                ax_val_th = round((js.get_axis(THROTTLE_AXIS)), 2)  # keep 2 decimals
+            elif e.type == pygame.JOYBUTTONDOWN:
+                if js.get_button(STOP_BUTTON):  # emergency stop 
+                    print("E-STOP PRESSED. TERMINATE")
+                    cv.destroyAllWindows()
+                    pygame.quit()
+                    ser_pico.close()
+                    sys.exit()
+        # Calaculate steering and throttle value
+        act_st = ax_val_st
+        act_th = -ax_val_th  # throttle action: -1: max forward, 1: max backward
+        # Encode steering value to dutycycle in nanosecond
+        duty_st = STEERING_CENTER - STEERING_RANGE + int(STEERING_RANGE * (act_st + 1))
+        # Encode throttle value to dutycycle in nanosecond
+        if act_th > 0:
+            duty_th = THROTTLE_STALL + int(THROTTLE_FWD_RANGE * min(act_th, THROTTLE_LIMIT))
+        elif act_th < 0:
+            duty_th = THROTTLE_STALL + int(THROTTLE_REV_RANGE * max(act_th, -THROTTLE_LIMIT))
+        else:
+            duty_th = THROTTLE_STALL 
+        msg = (str(duty_st) + "," + str(duty_th) + "\n").encode('utf-8')
+        # f"{act_st,act_th}\n".encode('utf-8')
+        ser_pico.write(msg)
+        # Log action
+        action = [act_st, act_th]
+        print(f"action: {action}")
+        # Log frame rate
+        frame_counts += 1
+        since_start = time() - start_stamp
+        frame_rate = frame_counts / since_start
+        print(f"frame rate: {frame_rate}")
+        # Press "q" to quit
+        if cv.waitKey(1)==ord('q'):
+            cv.destroyAllWindows()
+            pygame.quit()
+            ser_pico.close()
+            sys.exit()
+
+# Take care terminal signal (Ctrl-c)
+except KeyboardInterrupt:
+    pygame.quit()
+    ser_pico.close()
+    sys.exit()
